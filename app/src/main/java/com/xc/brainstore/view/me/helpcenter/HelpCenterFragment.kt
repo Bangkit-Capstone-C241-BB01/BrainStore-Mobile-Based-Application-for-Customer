0e575c10cb4dbdf0153e848c994bf12d37087596
CeciliaXu01 <107264970+CeciliaXu01@users.noreply.github.com>
2024-06-10 19:24:47 +0700

    commit

--- app/src/main/java/com/xc/brainstore/view/me/helpcenter/HelpCenterFragment.kt
+++ app/src/main/java/com/xc/brainstore/view/me/helpcenter/HelpCenterFragment.kt
@@ -5,23 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.xc.brainstore.R
 import com.xc.brainstore.databinding.FragmentHelpCenterBinding
 
 class HelpCenterFragment : Fragment() {
-    private var _binding: FragmentHelpCenterBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.progressBar.visibility = View.GONE
-    }
+    private lateinit var binding: FragmentHelpCenterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_help_center, container, false)
+    ): View {
+        binding = FragmentHelpCenterBinding.inflate(inflater, container, false)
+        binding.progressBar.visibility = View.GONE
+        return binding.root
     }
 
 }
